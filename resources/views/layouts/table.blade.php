52e369e8eefbd8347c2278bf6e328b835e8dfdd3
isna_na <narutost9@gmail.com>
2020-08-10 22:21:36 +0700

    Quiz3 pembaharuan table

--- resources/views/layouts/table.blade.php
+++ resources/views/layouts/table.blade.php
@@ -3,7 +3,12 @@
 <table class="table table-bordered dataTable" id="dataTable" width="100%" cellspacing="0" role="grid" aria-describedby="dataTable_info" style="width: 100%;">
                   <thead>
               
-                    <tr><th rowspan="1" colspan="1">ID</th><th rowspan="1" colspan="1">Nama Proyek</th>
+                    <tr><th rowspan="1" colspan="1">ID</th>
+                    <th rowspan="1" colspan="1">Nama Proyek</th>
+                    <th rowspan="1" colspan="1">Deskripsi Proyek</th>
+                    <th rowspan="1" colspan="1">Tangal Mulai</th>
+                    <th rowspan="1" colspan="1">Tanggal Selesai</th>
+                    <th rowspan="1" colspan="1">Status</th>
                   </tfoot>
                   <tbody>
                     
@@ -11,7 +16,10 @@
                   <tr role="row" class="odd">
                       <td class="sorting_1">{{ $p->idProyek }}</td>
                       <td>{{ $p->namaProyek }}</td>
-                  
+                      <td>{{ $p->descProyek }}</td>
+                      <td>{{ $p->tanggal_mulai }}</td>
+                      <td>{{ $p->tanggal_deadline }}</td>
+                      <td>{{ $p->status }}</td>
                      
                    </tr>
 
